c8da88d7941f5f55f0f7ce301c4904ccb3c4d1a4
CJ <treasure32109@gmail.com>
2025-10-11 10:34:46 +0100

    commision

---
+++ Week_1/mutables/main.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let x = 5;
+    let mut y = 10;
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    y = 15;
+    println!("The value of y changed, now it’s: {}", y);
+}
