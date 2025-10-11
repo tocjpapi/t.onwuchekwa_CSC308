862c020d1515de6158aa6ae3395e81738a737c49
CJ <treasure32109@gmail.com>
2025-10-11 17:22:48 +0100

    Create main.rs

---
+++ Week_1/Conditionals return values/main.rs
@@ -0,0 +1,6 @@
+fn main(){
+    let score = 85;
+    let grade = if score >= 90 {"Excellent"} else if score >= 75 {"Great"} else {"You can do better"};
+
+    println!("Your grade is: {}", grade);
+}
