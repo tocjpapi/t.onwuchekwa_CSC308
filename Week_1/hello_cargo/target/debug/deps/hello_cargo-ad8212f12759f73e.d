311adc5052a3cb46f76d862ce0c945435e1e9abc
CJ <treasure32109@gmail.com>
2025-10-11 10:24:38 +0100

    commision

---
+++ Week_1/hello_cargo/target/debug/deps/hello_cargo-ad8212f12759f73e.d
@@ -0,0 +1,5 @@
+/Users/cj/Documents/GitHub/t.onwuchekwa_CSC308/Week_1/hello_cargo/target/debug/deps/hello_cargo-ad8212f12759f73e.d: src/main.rs
+
+/Users/cj/Documents/GitHub/t.onwuchekwa_CSC308/Week_1/hello_cargo/target/debug/deps/hello_cargo-ad8212f12759f73e: src/main.rs
+
+src/main.rs:
---
+++ Week_1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/cj/Documents/GitHub/t.onwuchekwa_CSC308/Week_1/hello_cargo/target/debug/hello_cargo: /Users/cj/Documents/GitHub/t.onwuchekwa_CSC308/Week_1/hello_cargo/src/main.rs
