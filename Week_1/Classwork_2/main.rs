1e6ed121f1a675a79dea91a01977f542e6ef7461
CJ <treasure32109@gmail.com>
2025-10-11 19:06:29 +0100

    Update main.rs

--- Week_1/Classwork_2/main.rs
+++ Week_1/Classwork_2/main.rs
@@ -1,3 +1,22 @@
 fn main(){
+    let mut bill = String::new();
+    println!("Enter the total bill amount:");
+    std::io::stdin().read_line(&mut bill).expect("Failed to read line");
+    let bill: f64 = bill.trim().parse().expect("Please enter a valid number");
+
+    if bill > 5000.0 {
+        let discount = bill * 0.10;
+        let total = bill - discount;
+        println!("You get a 10% discount! Your total bill is: {:.2}", total);
+    }
+
+    else if bill > 10000.0{
+        let discount = bill * 0.15;
+        let total = bill - discount;
+        println!("You get a 15% discount! Your total bill is: {:.2}", total);  
+    }
     
+     else {
+        println!("No discount applied. Your total bill is: {:.2}", bill);
+    }
 }
