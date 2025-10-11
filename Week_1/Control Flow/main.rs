0fa3bacfb5a7111ab9a527a2e50bfde9caab35fe
CJ <treasure32109@gmail.com>
2025-10-11 17:01:25 +0100

    comission

---
+++ Week_1/Control Flow/main.rs
@@ -0,0 +1,12 @@
+fn main(){
+    let score = 85;
+    if score >= 90 {
+        println!("Excellent");
+    }
+    else if score >= 75 {
+        println!("Good");
+    }
+    else {
+        println!("Needs Improvement");
+    }
+}
