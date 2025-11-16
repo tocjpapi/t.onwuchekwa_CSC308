4b150645803948f6ee593d40e79753416be58b69
CJ <treasure32109@gmail.com>
2025-11-16 16:43:47 +0100

    ok

---
+++ Week_6/Example_1/main.rs
@@ -0,0 +1,10 @@
+fna main(){
+    let list =vec![1,2,3];
+    println!("Before defining closore: {list:?}");
+    
+    let only_borrows = || println("From closure: {list:?}");
+
+    println!("Before calling closure: {list:?}");
+    only_borrows();
+    println!("After calling closure: {list:?}")
+}
---
+++ Week_6/Task_1/main.rs
@@ -0,0 +1,25 @@
+//Number 1
+
+fn main(){
+    let factorial = |n: u64| -> u64 {
+    if n <= 1 {
+        1
+    }
+
+    else{
+        (1..=n).product()
+    }
+};
+
+   println!("{}",factorial(4));
+}
+
+//Number 2
+
+fn main(){
+    let vector = vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+    let even_check = || {
+        
+    }
+}
